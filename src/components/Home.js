1f52ded14909aa5fbf357452067d437a6c762068
unknown <aramdeveloper1@gmail.com>
2024-03-31 09:22:49 +0300

    commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,10 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return <div id="home">
+    <h1> Your Name is a Web Developer from { city }
+      <span style={ { color: "firebrick" } }>{ name }</span></h1>
+  </div >;
 }
 
 export default Home;
